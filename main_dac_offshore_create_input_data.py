e47be52dffa71a5f42570fb09fb0c4f567ef2a8a
jwiegner <jwiegner@ethz.ch>
2026-07-24 16:37:19 +0200

    Clean up

---
+++ main_dac_offshore_create_input_data.py
@@ -0,0 +1,6 @@
+from dac_offshore.create_input_data import InputDataCreator
+
+" CHANGE TEST, BASELINE EMISSIONS AND MAX NEGATIVE EMISSIONS FOR CORRECT SOLVE "
+
+input_data_creator = InputDataCreator()
+input_data_creator.create_input_data()
